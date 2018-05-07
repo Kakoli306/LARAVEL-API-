528e99c3a1a65752d3aa992bca38f83ff9fb51c5
kakoli Saha <kakoli1compete@gmail.com>
2018-05-08 01:02:34 +0600

    Laravel 5.5 API From Scratch Using Resources

---
+++ resources/assets/js/components/Articles.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+<h2>Articles</h2>
+<div class="card card-body mb-2" v-for="article in articles" v-bind:key="article.id">
+
+</div>
+</div>
+
+</template>
+
+
+    
+<script>
+export default {
+  data() {
+    return {
+      articles: [],
+      article: {
+        id: '',
+        title: '',
+        body: ''
+      },
+      article_id: '',
+      pagination: {},
+      edit: false
+    };
+  },
+  created() {
+    this.fetchArticles();
+  },
+  methods: {
+        fetchArticles() {
+            fetch('api/articles')
+            .then(res => res.json())
+            .then(res => {
+                console.log(res.data);
+            });
+        }
+    }
+};
+
+
+</script>
+
